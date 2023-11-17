6a58a2535cbf6a5bb19ff8efd043785ff7c7090a
raghav goyal <raghavgoyaldbd4782gmail.com>
2023-11-17 16:35:16 +0530

    changes in validate.js

--- src/util/validate.js
+++ src/util/validate.js
@@ -1,13 +1,12 @@
 export const isValidate = (email, password) => {
-    const isValidEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
-    const isValidPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
-  
-    if (!isValidEmail) {
-      return "Email is not valid";
-    }
-    if (!isValidPassword) {
-      return "Password is not valid";
-    }
-    return null;
-  };
-  
+  const isValidEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+  const isValidPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(password);
+
+  if (!isValidEmail) {
+    return "Email is not valid";
+  }
+  if (!isValidPassword) {
+    return "Password is not valid";
+  }
+  return null;
+};
